ab27a928d658d9455798cbd16b8cef072bb432d1
shaurya21343 <smms1917@gmail.com>
2026-01-08 23:08:46 +0530

    deploy frontend

--- frontend/src/socket.ts
+++ frontend/src/socket.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = "http://localhost:3000"; // backend URL
+const SOCKET_URL = import.meta.env.VITE_BackendURI; // backend URL
 
 export const socket: Socket = io(SOCKET_URL, {
   autoConnect: false,   // connect manually
